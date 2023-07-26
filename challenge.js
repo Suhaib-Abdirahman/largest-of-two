f694db8e79971dd0e1cf7efde2952a59e9aa1626
Suhaib Abdirahman <139591958+Suhaib-Abdirahman@users.noreply.github.com>
2023-07-26 15:43:31 +0300

    Completed

--- challenge.js
+++ challenge.js
@@ -1,2 +1,17 @@
 // Good Luck! You got this 💪🏾
 // Write your code here.
+
+function findLargest(num1, num2) {
+    if (num1 > num2) {
+      return num1;
+    } else {
+      return num2;
+    }
+  }
+  
+  var num1 = 20;
+  var num2 = 10;
+  
+  var largestNum = findLargest(num1, num2);
+  
+  console.log("The largest number is: " + largestNum);
